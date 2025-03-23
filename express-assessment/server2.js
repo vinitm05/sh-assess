7f80f0558eebda4700f425eafa741c63342adf40
Vinit Mittal <vinitmittal14@gmail.com>
2025-03-23 09:56:35 +0530

    handling unknown routes

---
+++ express-assessment/server2.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/products', (req, res) => {
+    res.send('Here is the lsit of all products');
+})
+
+app.post("/products", (req, res) => {
+  res.send("A new product has been added");
+});
+
+app.get("/categories", (req, res) => {
+  res.send("Here is the lsit of all categories");
+});
+
+app.post("/categories", (req, res) => {
+  res.send("A new category has been created");
+});
+
+app.get('/*', (req, res) => {
+    res.status(404).send("<h1>404-Page Not Found</h1>")
+})
+app.listen(4000, () => {
+    console.log("Listening on port 4000");
+})
