e454e10c2235ef8595d9c37ba1b5c2bba236b4ee
Vinit Mittal <vinitmittal14@gmail.com>
2025-03-23 09:44:33 +0530

    working with different routes

--- express-assessment/server.js
+++ express-assessment/server.js
@@ -3,12 +3,28 @@ const addProperty = require("./middlewares/addProperty");
 
 const app = express();
 
-app.get('/welcome', addProperty, (req, res) => {
-    res.send(`<h1>Welcome, ${req.user}!</h1>`)
-})
+app.get("/welcome", addProperty, (req, res) => {
+  res.send(`<h1>Welcome, ${req.user}!</h1>`);
+});
+
+app.get("/orders", (req, res) => {
+  res.send("Here is the list of all orders.");
+});
+
+app.post("/orders", (req, res) => {
+  res.send("A new order has been created.");
+});
+
+app.get("/users", (req, res) => {
+  res.send("Here is the list of all users.");
+});
+
+app.post("/users", (req, res) => {
+  res.send("A new user has been added.");
+});
 
 app.listen(3000, () => {
-    console.log(
-      "Server is up and running on port 3000! Ready to handle requests."
-    );
-})
+  console.log(
+    "Server is up and running on port 3000! Ready to handle requests."
+  );
+});
